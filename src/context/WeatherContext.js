07e2c0c91663ec564ef4f366149eceec20b4a1d3
Ramazan <ruslu1720@gmail.com>
2022-05-15 18:38:19 +0300

    key created

--- src/context/WeatherContext.js
+++ src/context/WeatherContext.js
@@ -14,7 +14,7 @@ export const WeatherProvider = ({ children }) => {
 
   const WeatherApi = async () => {
     const url = "https://api.openweathermap.org/data/2.5/";
-    const key = "ff########################";
+    const key = "ffd5b4813a7673da9f61e3e5ff815359";
     const { data } = await axios.get(
       `${url}onecall?lat=${city.latitude}&lon=${city.longitude}&units=metric&exclude=current,minutely,hourly,alerts&lang=tr&appid=${key}`
     );
